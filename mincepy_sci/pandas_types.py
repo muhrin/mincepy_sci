805c6f034dda1a25943a15d9e5ed763b4fb3bf39
Martin Uhrin <martin.uhrin@gmail.com>
2021-07-30 12:06:06 +0200

    Fixed pandas eq() and sped up hash
    The equals method was not implemented as was therefore falling back to
    the equals operator which is not defined for dataframes.  Now we,
    correctly, use df.equals(other).
    Hash has been changed to use a hash of the numpy array bytes
    representation of a DF.  This is much faster and should give good
    results.

--- mincepy_sci/pandas_types.py
+++ mincepy_sci/pandas_types.py
@@ -8,12 +8,18 @@ else:
 
     import mincepy
 
-    class DataFrameHelper(mincepy.BaseHelper):
+    class DataFrameHelper(mincepy.TypeHelper):
         TYPE = pandas.DataFrame
         TYPE_ID = uuid.UUID('0fc8dc7b-7378-4e5a-ba1f-ad8dcb0dd3c8')
 
+        def eq(self, one: pandas.DataFrame, other: pandas.DataFrame) -> bool:
+            if not isinstance(one, pandas.DataFrame) or not isinstance(other, pandas.DataFrame):
+                return False
+
+            return one.equals(other)
+
         def yield_hashables(self, obj, hasher):
-            yield from hasher.yield_hashables(obj.to_dict())
+            yield from hasher.yield_hashables(obj.values.tobytes())
 
         def save_instance_state(self, obj: pandas.DataFrame, saver):  # pylint: disable=unused-argument
             return obj.to_dict(orient='split')
