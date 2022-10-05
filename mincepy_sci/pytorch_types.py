d09911c1a05968361cd065c7b2437ec9eca06fe0
Martin Uhrin <martin.uhrin@gmail.com>
2022-10-05 14:23:29 +0200

    Improvements to pytorch types
    * Fixed ModuleHelperSub (was using save_state() which doesn't contain
      enough information for mincepy)
    * Added a few commonly used modules...more to come soon.

--- mincepy_sci/pytorch_types.py
+++ mincepy_sci/pytorch_types.py
@@ -7,7 +7,6 @@ import bidict
 import mincepy
 import torch
 
-
 # pylint: disable=no-member
 torch_dtypes = bidict.bidict(
     {
@@ -62,24 +61,35 @@ class TensorHelper(mincepy.BaseHelper):
 
 
 class ModuleHelperStub(mincepy.BaseHelper):
-    """This stub needs to be implemented for each model type that you want to load with the
-    TYPE and TYPE_ID set uniquely"""
 
     # pylint: disable=arguments-differ
 
     def yield_hashables(self, module: torch.nn.Module, hasher):
-        yield from hasher.yield_hashables(module.state_dict())
+        yield from hasher.yield_hashables(module.__dict__)
 
     def save_instance_state(self, model: torch.nn.Module, _saver):
-        return dict(model.state_dict())
+        return model.__dict__
 
     def load_instance_state(
         self, module: torch.nn.Module, saved_state: dict, _referencer
     ):
-        # Need to call at least the Module constructor so that it creates some necessary members
-        module.__init__()
-        module.load_state_dict(saved_state)
-        module.eval()
+        for key, value in saved_state.items():
+            setattr(module, key, value)
+
+
+class Conv2dHelper(ModuleHelperStub):
+    TYPE = torch.nn.Conv2d
+    TYPE_ID = uuid.UUID("bbdb255b-e59b-4968-960d-404f16041379")
+
+
+class MaxPool2dHelper(ModuleHelperStub):
+    TYPE = torch.nn.MaxPool2d
+    TYPE_ID = uuid.UUID("eaf92947-98c2-4277-a38a-bc1e30429056")
+
+
+class LinearHelper(ModuleHelperStub):
+    TYPE = torch.nn.Linear
+    TYPE_ID = uuid.UUID("2c8c1e39-aa20-4095-81ef-b8e780403e53")
 
 
 class SavableModuleMixin(mincepy.SavableObject):
@@ -88,15 +98,14 @@ class SavableModuleMixin(mincepy.SavableObject):
     # pylint: disable=arguments-differ
 
     def yield_hashables(self: torch.nn.Module, hasher):
-        yield from hasher.yield_hashables(self.state_dict())
+        yield from hasher.yield_hashables(self.__dict__)
 
     def save_instance_state(self: torch.nn.Module, _saver) -> dict:
-        return dict(self.state_dict())
+        return self.__dict__
 
     def load_instance_state(self: torch.nn.Module, saved_state: dict, _referencer):
-        self.__init__()
-        self.load_state_dict(saved_state)
-        self.eval()
+        for key, value in saved_state.items():
+            setattr(self, key, value)
 
 
-TYPES = (TensorHelper(),)
+TYPES = (TensorHelper, Conv2dHelper, MaxPool2dHelper, LinearHelper)
